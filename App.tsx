c010d41df3711a8fd5e6ebf025cb95f72fbb96fd
Elvis <elvis.e.amancio@gmail.com>
2022-01-05 20:51:45 -0300

    Criação SignIn

--- App.tsx
+++ App.tsx
@@ -6,8 +6,8 @@ import { ThemeProvider } from 'styled-components/native'
 import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans';
 import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
 
-import Teste from '@screens/Teste';
 import theme from './src/theme'
+import SignIn from '@screens/SignIn';
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -21,9 +21,8 @@ export default function App() {
     return (
       <>
         <ThemeProvider theme={theme}>
-
           <StatusBar style="light" />
-          <Teste />
+          <SignIn />
         </ThemeProvider>
       </>
     );
